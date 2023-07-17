555489eb3557d9b48205eeb3a045fe056c949f45
Matt Teichman <teichman@uchicago.edu>
2023-07-17 09:02:12 -0500

    put the subservice into Lib.GetBrowseListLanguages

--- collections.ml
+++ collections.ml
@@ -1,26 +1,16 @@
 open Restful
 
-(* open Lib.Fetch *)
-
 module Service = struct
   include Service
-  (* open Valid *)
   open Param
   module D = Lib.Defaults
 
-  let getBrowseListLanguages cgi _ _ =
-    let open Lib.GetBrowseListLanguages in
-    let spec = Spec.spec in
-    let ps = process cgi spec in
-    let group = (value ps "group") in
-    let collection = (value ps "collection") in
-    Content.write ~content_type:"text/plain" cgi
-      (Printf.sprintf "%s" (gimme ~group ~collection ()))
+  let endpoints = [
+      Lib.GetBrowseListLanguages.(endpoint_name, subservice);
+    ]
 
   let main _ _ cgi =
-    Pathinfo.dispatch "multi" ["getBrowseListLanguages", getBrowseListLanguages] cgi
-
-
+    Pathinfo.dispatch "endpoint" endpoints cgi
 end
 
 module R = Make (Service) (Error (Log.Default))
--- lib/getBrowseListLanguages/getBrowseListLanguages.ml
+++ lib/getBrowseListLanguages/getBrowseListLanguages.ml
@@ -11,6 +11,8 @@ module Params = struct
     in of_list qs
 end
 
+let endpoint_name = Params.endpoint_name
+
 module Url = struct
   let url
         ?(curl=true)
@@ -125,6 +127,18 @@ module Spec = struct
       "collection", Mandatory, notblank;
     ]
 end
+
+
+let subservice cgi _ _ =
+  let open Restful in
+  let open Param in
+  let spec = Spec.spec in
+  let ps = process cgi spec in
+  let group = (value ps "group") in
+  let collection = (value ps "collection") in
+  Content.write ~content_type:"text/plain" cgi
+    (Printf.sprintf "%s" (gimme ~group ~collection ()))
+
 
 module Testing = struct
   let string = {|{"head":{"vars":["prefLabel","code"]},"results":{"bindings":[{"prefLabel":{"xml:lang":"en","type":"literal","value":"Aguacateco"},"code":{"type":"literal","value":"agu"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Akateko"},"code":{"type":"literal","value":"knj"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Buglere"},"code":{"type":"literal","value":"sab"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Central Guerrero Nahuatl"},"code":{"type":"literal","value":"ngu"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Central Huasteca Nahuatl"},"code":{"type":"literal","value":"nch"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Chol"},"code":{"type":"literal","value":"ctu"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Chuj"},"code":{"type":"literal","value":"cac"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Classical Nahuatl"},"code":{"type":"literal","value":"nci"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"English"},"code":{"type":"literal","value":"eng"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Garifuna"},"code":{"type":"literal","value":"cab"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"German"},"code":{"type":"literal","value":"deu"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Highland Totonac"},"code":{"type":"literal","value":"tos"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Huastec"},"code":{"type":"literal","value":"hus"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Huautla Mazatec"},"code":{"type":"literal","value":"mau"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Huichol"},"code":{"type":"literal","value":"hch"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Ixcatec"},"code":{"type":"literal","value":"ixc"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Ixil"},"code":{"type":"literal","value":"ixl"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"K'iche'"},"code":{"type":"literal","value":"quc"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Kekchí"},"code":{"type":"literal","value":"kek"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Mam"},"code":{"type":"literal","value":"mam"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Mayo"},"code":{"type":"literal","value":"mfy"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Mezquital Otomi"},"code":{"type":"literal","value":"ote"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Mocho"},"code":{"type":"literal","value":"mhc"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Mopán Maya"},"code":{"type":"literal","value":"mop"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Ngäbere"},"code":{"type":"literal","value":"gym"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Pipil"},"code":{"type":"literal","value":"ppl"}},{"prefLabel":{"xml:lang":"es","type":"literal","value":"Pipil"},"code":{"type":"literal","value":"ppl"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Popti'"},"code":{"type":"literal","value":"jac"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Poqomam"},"code":{"type":"literal","value":"poc"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Poqomam, Eastern"},"code":{"type":"literal","value":"poa"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Poqomchi'"},"code":{"type":"literal","value":"poh"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Poqomchi', Western"},"code":{"type":"literal","value":"pob"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Purepecha"},"code":{"type":"literal","value":"tsz"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Q'anjob'al"},"code":{"type":"literal","value":"kjb"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Spanish"},"code":{"type":"literal","value":"spa"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Tlaxcala-Puebla-Central Nahuatl"},"code":{"type":"literal","value":"nhn"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Tojolabal"},"code":{"type":"literal","value":"toj"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Tzeltal"},"code":{"type":"literal","value":"tzh"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Tzotzil"},"code":{"type":"literal","value":"tzo"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Tzotzil, Zinacantán"},"code":{"type":"literal","value":"tzz"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Yucatec Maya"},"code":{"type":"literal","value":"yua"}},{"prefLabel":{"xml:lang":"en","type":"literal","value":"Zapotec"},"code":{"type":"literal","value":"zap"}}]}}|}
