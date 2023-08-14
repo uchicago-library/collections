068a78d86e2819992d823a71a109e31a0b463731
Matt Teichman <teichman@uchicago.edu>
2023-08-14 11:03:27 -0500

    added printing.mli back in

---
+++ lib/printing.mli
@@ -0,0 +1,13 @@
+module Json : sig
+  val to_string : Data_encoding.Json.json -> string
+  val print : ?truncate:int -> Data_encoding.Json.json -> unit
+end
+
+module Encoding : sig
+  val to_string : 'a Data_encoding.Encoding.t -> string
+  val print : ?truncate:int -> 'a Data_encoding.Encoding.t -> unit
+end
+
+module Error : sig
+  val to_string : exn -> string
+end
