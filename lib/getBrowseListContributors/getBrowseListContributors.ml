4d81bd9377983df18f21035c0e4b0cc2bb7f3f40
Matt Teichman <teichman@uchicago.edu>
2023-08-18 13:55:02 -0500

    oops, forgot to commit getBrowseListContributors directory

---
+++ lib/getBrowseListContributors/getBrowseListContributors.ml
@@ -0,0 +1,26 @@
+open Prelude
+module D = Defaults
+module R = Mattlude.Endofunctors.Result.Make (String)
+
+module Params = struct
+  include Url.Querystring
+
+  let endpoint_name = "getBrowseListContributors"
+
+  let make
+        ?(collection=D.collection)
+        ?identifier
+        ?search
+        () =
+    let _ = identifier in
+    let _ = search in
+    let qs = [ ("collection", [collection]) ]
+    in Dict.of_list Dict.empty qs
+
+  let to_list = Dict.to_list 
+end
+
+let endpoint_name = Params.endpoint_name
+
+include Utils.Fetcher (Params) (D)
+
